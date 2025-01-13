bff901f9d0ef2fed499395bc1f4e5ad0538f0ac3
Rohankhokhar <167191892+Rohankhokhar@users.noreply.github.com>
2025-01-13 14:44:58 +0530

    Add project files (excluding env/)

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('snippets/', snippet_list, name='snippet_list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet_detail'),
+    path('snippets/<int:snippet_id>/comments/', comment_list, name='comment_list'),
+]
